8600363c26c23afffca3387095a6c0e67734a795
heswap <heswap@protonmail.com>
2021-06-22 16:20:01 +0800

    Done Farm pages

--- src/views/Farms/components/ToggleView/ToggleView.tsx
+++ src/views/Farms/components/ToggleView/ToggleView.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import styled from 'styled-components'
-import { ListViewIcon, CardViewIcon, IconButton } from '@pancakeswap/uikit'
+import { ListViewIcon, CardViewIcon, IconButton } from '@heswap/uikit'
 import { ViewMode } from '../types'
 
 interface ToggleViewProps {
