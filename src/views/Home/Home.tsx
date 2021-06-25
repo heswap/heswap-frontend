7fb57175d129fd77ac86af9e7a036f5357270b3c
heswap <heswap@protonmail.com>
2021-06-25 11:00:32 +0800

    refactor: Rebrand index.html

--- src/views/Home/Home.tsx
+++ src/views/Home/Home.tsx
@@ -90,9 +90,9 @@ const Home: React.FC = () => {
     <Page>
       <Hero>
         <Heading as="h1" scale="xl" mb="24px" color="secondary">
-          {t('PancakeSwap')}
+          {t('Heswap')}
         </Heading>
-        <Text>{t('The #1 AMM and yield farm on Binance Smart Chain.')}</Text>
+        <Text>{t('The First DEX on BSC network with the lowest fees in Defi.')}</Text>
       </Hero>
       <div>
         <Cards>
