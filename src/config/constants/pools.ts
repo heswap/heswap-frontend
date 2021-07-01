2e1e6cf41f62866ca76f3593a66e2e5966b95cd7
heswap <heswap@protonmail.com>
2021-07-01 14:33:30 +0800

    refactor: Update contractAddress when sousId =0

--- src/config/constants/pools.ts
+++ src/config/constants/pools.ts
@@ -7,8 +7,8 @@ const pools: PoolConfig[] = [
     stakingToken: tokens.cake,
     earningToken: tokens.cake,
     contractAddress: {
-      97: '0xd3af5fe61dbaf8f73149bfcfa9fb653ff096029a',
-      56: '0x73feaa1eE314F8c655E354234017bE2193C9E24E',
+      97: '0x2CBa4E01838651EbA879B4eF9f0B2E5Cb81386Be',
+      56: '',
     },
     poolCategory: PoolCategory.CORE,
     harvest: true,
