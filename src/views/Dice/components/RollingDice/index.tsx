c8fdaf03fb25cdf90377d9bc64b2c0430d9162d7
bitfriend <bitfriend@hotmail.com>
2021-07-08 01:59:13 +0200

    Implement one time animation in dice page

--- src/views/Dice/components/RollingDice/index.tsx
+++ src/views/Dice/components/RollingDice/index.tsx
@@ -1,7 +1,54 @@
-import React from 'react'
+import React, { useRef } from 'react'
+import { noop } from 'lodash'
+import { keyframes } from 'styled-components'
 import './styles.css'
 
+function makeRandomTransform() {
+  const rnd = Math.floor(Math.random() * 6 + 1)
+  let x
+  let y
+  switch (rnd) {
+    case 1:
+      x = 720
+      y = 810
+      break
+    case 6:
+      x = 720
+      y = 990
+      break
+    default:
+      x = 720 + (6 - rnd) * 90
+      y = 900
+      break
+  }
+  return `translateZ(-100px) rotateY(${x}deg) rotateX(${y}deg)`
+}
+
 const RollingDice: React.FC = () => {
+  const cubeRef = useRef(null)
+
+  const handleClick = (evt: React.MouseEvent<HTMLElement>) => {
+    evt.preventDefault()
+    const rnd = Math.floor(Math.random() * 6 + 1)
+    let x
+    let y
+    switch (rnd) {
+      case 1:
+        x = 720
+        y = 810
+        break
+      case 6:
+        x = 720
+        y = 990
+        break
+      default:
+        x = 720 + (6 - rnd) * 90
+        y = 900
+        break
+    }
+    cubeRef.current.style.transform = `translateZ(-100px) rotateY(${x}deg) rotateX(${y}deg)`
+  }
+
   return (
     <>
       <div id="background" />
@@ -10,8 +57,8 @@ const RollingDice: React.FC = () => {
         <input id="roll" name="roll" type="checkbox" />
         {/* <label for="roll">Roll it!</label>
         <label for="secondroll"><span>Stop!</span></label> */}
-        <div id="platform">
-          <div id="dice">
+        <div id="platform" role="button" tabIndex={0} onClick={handleClick} onKeyPress={noop}>
+          <div id="dice" ref={cubeRef}>
             <div className="side front">
               <div className="dot center" />
             </div>
