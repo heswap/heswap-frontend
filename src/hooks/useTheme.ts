6da8025ae0931a970cc071ee732e847244d13297
bitfriend <bitfriend@hotmail.com>
2021-07-22 14:41:33 +0200

    Implement green theme

--- src/hooks/useTheme.ts
+++ src/hooks/useTheme.ts
@@ -1,27 +1,27 @@
 import { useContext } from 'react'
 import { ThemeContext as StyledThemeContext } from 'styled-components'
 import { ThemeContext } from 'contexts/ThemeContext'
-import { blue, indigo, purple, pink, red, orange, yellow, green, teal, cyan, gray } from 'utils/palette'
+import { red, pink, purple, deepPurple, indigo, blue, lightBlue, cyan, teal, green, lightGreen, lime, yellow, amber, orange, deepOrange, brown, grey, blueGrey } from 'utils/palette'
 
 const useTheme = () => {
   const { isDark, toggleTheme } = useContext(ThemeContext)
   const theme = useContext(StyledThemeContext)
-  theme.colors.primary = teal[500]
-  theme.colors.secondary = pink[500]
-  theme.colors.success = cyan[500]
-  theme.colors.warning = orange[500]
-  theme.colors.background = teal[800]
+  theme.colors.primary = teal[400]
+  theme.colors.secondary = red[400]
+  theme.colors.success = cyan[400]
+  theme.colors.warning = amber[400]
+  theme.colors.background = teal[900]
   theme.colors.backgroundDisabled = teal[100]
   theme.colors.dropdown = teal[900]
-  theme.colors.tertiary = teal[600]
+  theme.colors.tertiary = teal[100]
   theme.colors.inputBorder = teal[700]
   theme.colors.inputFocusedBorder = teal[600]
-  theme.colors.text = gray[600]
-  theme.colors.textDisabled = gray[400]
+  theme.colors.text = grey[500]
+  theme.colors.textDisabled = grey[400]
   theme.colors.textSubtle = teal[200]
-  theme.colors.gradients.pageHeader = `linear-gradient(180deg, ${teal[700]}, ${teal[500]})`
+  theme.colors.gradients.pageHeader = `linear-gradient(180deg, ${teal[700]}, ${teal[400]})`
   theme.colors.gradients.cardHeader = `linear-gradient(180deg, ${teal[800]}, ${teal[600]})`
-  theme.colors.gradients.slickDotLoading = `linear-gradient(270deg, ${orange[400]}, ${orange[600]})`
+  theme.colors.gradients.slickDotLoading = `linear-gradient(270deg, ${amber[400]}, ${amber[600]})`
   theme.colors.gradients.homeCard = `linear-gradient(235deg, ${teal[400]} 4.05%, ${teal[600]} 103.52%)`
   theme.menu.topBarColor = teal[900]
   theme.menu.leftBarColor = teal[700]
